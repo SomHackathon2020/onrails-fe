5761dc88d124d6f5d9a77066e14303089592d223
Albert Loma <aloma@edu.tencocampus.cat>
2020-02-08 12:58:46 +0100

    added user model

--- on_rails_app/lib/routes/routes.dart
+++ on_rails_app/lib/routes/routes.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:on_rails_app/pages/homePage.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
 
   return <String, WidgetBuilder> {
-     // '/'      : ( BuildContext context ) => HomePage(),
+      '/'      : ( BuildContext context ) => HomePage(),
   };
 
 }
