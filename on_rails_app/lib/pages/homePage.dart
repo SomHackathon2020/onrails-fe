e3beea07457cb070404962d3b40ac92e4a593c02
Albert Loma <aloma@edu.tencocampus.cat>
2020-02-08 16:28:46 +0100

    event card implemented

--- on_rails_app/lib/pages/homePage.dart
+++ on_rails_app/lib/pages/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:on_rails_app/components/actividadComponent.dart';
 import 'package:on_rails_app/components/bottomNav.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,11 +8,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: const Text('Title App'),
-    ),
-    body: Center(
-      child: Text('xdxdxdxd')
+    //appBar: AppBar(
+    //  title: const Text('Title App'),
+    //),
+    body: ListView(
+      children: <Widget>[
+        EventComponent()
+      ],
     ),
     bottomNavigationBar: BottomNav(),
   );
