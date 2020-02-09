884e47cf8e60574f7f312e4697adc2c1b8836baf
Albert Loma <aloma@edu.tencocampus.cat>
2020-02-09 01:00:07 +0100

    kasndkjn

--- on_rails_app/lib/pages/activitiesPage.dart
+++ on_rails_app/lib/pages/activitiesPage.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:on_rails_app/components/actividadComponent.dart';
 import 'package:on_rails_app/components/actividadComponent2.dart';
 import 'package:on_rails_app/components/actividadComponent3.dart';
+import 'package:on_rails_app/providers/geolocator.dart';
 
 import '../components/actividadComponent.dart';
 
 class ActivityPage extends StatelessWidget {
 
+  GeolocatorService geolocatorService;
+
   @override
   Widget build(BuildContext context) {
+
+    geolocatorService = GeolocatorService();
     return SafeArea(
           child: Scaffold
           
@@ -19,7 +25,18 @@ class ActivityPage extends StatelessWidget {
            children: <Widget>[
               EventComponent(),
               EventComponent2(),
-              EventComponent3()
+              EventComponent3(),
+              FutureBuilder(
+                future: geolocatorService.getPos(),
+                builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+                  if(snapshot.hasData){
+                    return Text("");
+                    
+                  }else{
+
+                  }
+                },
+              ),
            ],
         ),
       ),
